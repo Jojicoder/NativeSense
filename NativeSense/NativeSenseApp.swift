21f3d1cdfd5a37a3b00e793cf452cf9a79a27874
Jojicoder <joji.kashimura@live.lagcc.cuny.edu>
2026-04-29 23:33:28 -0400

    Initial Commit

---
+++ NativeSense/NativeSenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeSenseApp.swift
+//  NativeSense
+//
+//  Created by Joji Kashimura on 4/29/26.
+//
+
+import SwiftUI
+
+@main
+struct NativeSenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NativeSenseTests/NativeSenseTests.swift
@@ -0,0 +1,19 @@
+//
+//  NativeSenseTests.swift
+//  NativeSenseTests
+//
+//  Created by Joji Kashimura on 4/29/26.
+//
+
+import Testing
+@testable import NativeSense
+
+struct NativeSenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
